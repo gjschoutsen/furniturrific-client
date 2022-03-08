1c2f3a77b01cf053ef3fca3e4b0c441d7798fb0c
GJ <gjschoutsen@tutanota.com>
2022-03-08 22:31:47 +0100

    added shopping cart total to the checkout page added some basic css

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,26 +1,25 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Form from "../components/Form";
-import Axios from 'axios';
-import './css/Checkout.css'
+import Axios from "axios";
+import "./css/Checkout.css";
 export default function CheckOut({ cartItems }) {
-  const [ user, setUser]= useState([])
+  const [user, setUser] = useState([]);
   const [cartFromStorageState, setCartFromStorageState] = useState([]);
+
+  const totalPrice = cartFromStorageState?.reduce((price, item) => {
+    return price + item.quantity * item.price;
+  }, 0);
 
   useEffect(() => {
     const cartFromStorage = JSON.parse(localStorage.getItem("cart"));
     setCartFromStorageState(cartFromStorage);
   }, [cartItems]);
 
-  return (
-    <div className="checkout-body">
-
-      <div className="add-address">
-        {/* <Form /> */}
-      </div>
-
-      <div className="show-address">
-        {user.map((userDetails)=>{
-          return(
+  const renderAddress = () => {
+    return (
+      <div className="render-address">
+        {user.map((userDetails) => {
+          return (
             <div className="user-details">
               <div>{userDetails.username}</div>
               <div>{userDetails.address.street}</div>
@@ -29,25 +28,41 @@ export default function CheckOut({ cartItems }) {
               <div>{userDetails.address.state}</div>
               <div>{userDetails.address.country}</div>
             </div>
-          )
+          );
         })}
       </div>
+    );
+  };
 
-    <div className="checkout-items">
-      {cartFromStorageState.map((e) => {
-        return (
-          <div className="checkout-container" key={e._id}>
-            <div>
-            <img src={e.image} alt="" />
+  const renderCartItems = () => {
+    return (
+      <div>
+        {cartFromStorageState.map((e) => {
+          return (
+            <div className="checkout-container" key={e._id}>
+              <div>
+                <p>{e.name}</p>
+              </div>
+              <div className="quant-price">
+                <div>{e.quantity}</div>
+                <div>€ {e.price},-</div>
+              </div>
             </div>
-            <div>
-              <h3>{e.name}</h3>
-            </div>
-            <div>{e.quantity}</div>
-            <div>Price: {e.price}</div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <div className="checkout-body">
+      <div className="add-address">{/* <Form /> */}</div>
+      {renderAddress()}
+      <div className="checkout-items">
+        <h5>Your Order</h5>
+        {renderCartItems()}
+        <div className="checkout-total-price">Total: €{totalPrice},-</div>
+        <button>Pay</button>
       </div>
     </div>
   );
